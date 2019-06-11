ab2bc2f4d6667b4a99d24e64e89968e5c58e2909
Ben R <brucker117@gmail.com>
2019-06-11 19:53:47 -0400

    add guildinfo class

---
+++ guild_info.py
@@ -0,0 +1,10 @@
+
+
+
+class GuildInfo():
+    """This class holds information about the state of the bot in a given guild."""
+
+
+    def __init__(self, id):
+        self.id = id
+        self.snapping = False
