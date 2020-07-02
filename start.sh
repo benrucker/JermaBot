a79c2d608efdceb5a4d90a2c87fe383b0d2bc404
Ben Rucker <brucker117@gmail.com>
2020-07-02 12:57:33 -0400

    add missing newline backslash

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ cd jerma
 while true
 do python3 jerma.py -mycroft /home/chamber/github/mimic1/mimic \
 	-mv ap \
-	-jv /home/chamber/open_jtalk/MMDAgent_Example-1.8/Voice/mei/mei_normal.htsvoice
+	-jv /home/chamber/open_jtalk/MMDAgent_Example-1.8/Voice/mei/mei_normal.htsvoice \
 	-jd /home/chamber/open_jtalk/open_jtalk_dic_utf_8-1.11
 sleep 5
 done
