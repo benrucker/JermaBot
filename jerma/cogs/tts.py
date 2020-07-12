a9dfa876a541e5f147bad5bb1399e8970f27f8e9
Ben Rucker <brucker117@gmail.com>
2020-07-12 15:57:24 -0400

    factor out duplicated code

--- jerma/cogs/tts.py
+++ jerma/cogs/tts.py
@@ -19,6 +19,9 @@ class TTS(commands.Cog):
         self.tts: ttsengine.TTSEngineInterface = tts_engine
         self.jtts: ttsengine.TTSEngineInterface = jtts_engine
 
+    async def connect(self, ctx):
+        return await self.bot.get_cog('Control').connect_to_user(ctx)
+
     def text_to_wav(self, text, speed='normal', engine=None):
         if not engine: engine = self.tts
         if speed == 'slow':
@@ -27,10 +30,13 @@ class TTS(commands.Cog):
             return engine.text_to_wav_fast(text)
         return engine.text_to_wav_normal(text)
 
-    def play_text(self, vc, to_speak, speed='normal', engine=None):
-        if not engine: engine = self.tts
-        sound_file = self.text_to_wav(to_speak, speed=speed, engine=engine)
+    def play_sound_file(self, sound_file, vc):
         self.bot.get_cog('SoundPlayer').play_sound_file(sound_file, vc)
+
+    def make_tts_sound_file(self, to_speak, speed='normal', engine=None):
+        if not engine: engine = self.tts
+        filepath = self.text_to_wav(to_speak, speed=speed, engine=engine)
+        return filepath
 
     def remove_leading_silence(self, sound, silence_threshold=-50.0, chunk_size=10):
         """
@@ -53,16 +59,18 @@ class TTS(commands.Cog):
     def generate_id_path(self, label, ctx):
         return os.path.join(self.bot.path, 'resources', 'soundclips', 'temp', label + str(ctx.guild.id) + '.wav')
 
-    def birthday_wave(self, name, ctx):
+    def birthday_sound(self, name_sound, ctx):
         """Construct birthday sound."""
         song = AudioSegment.from_file(os.path.join('resources', 'soundclips', 'blank_birthday.wav'))
-        name = AudioSegment.from_file(name)
-        name = self.remove_leading_silence(name)
+        name_sound = AudioSegment.from_file(name_sound)
+        name_sound = self.remove_leading_silence(name_sound)
+
         insert_times = [7.95 * 1000, 12.1 * 1000]
-
         for insert_time in insert_times:
-            song = song.overlay(name, position=insert_time)
+            song = song.overlay(name_sound, position=insert_time)
 
+        # TODO factor out an id path generator in utils
+        # or maybe be directed to one on construction
         outpath = self.generate_id_path('birthday_name', ctx)
         song.export(outpath)
         return outpath
@@ -73,24 +81,26 @@ class TTS(commands.Cog):
         print('new:', to_remove.sub('', text))
         return to_remove.sub('', text)
 
+    def convert_args_to_tts_text(self, args: list):
+        return self.strip_quotes(' '.join(args))
+
     @commands.command()
     async def birthday(self, ctx, *args):
         """Wish someone a happy birthday!"""
         if not args:
             raise discord.InvalidArgument()
-        to_speak = self.strip_quotes(' '.join(args))  # ingest args
-        name = self.text_to_wav(to_speak)
-        birthday_with_name = self.birthday_wave(name, ctx)
-        vc = await self.bot.get_cog('Control').connect_to_user(ctx)
-        player = self.bot.get_cog('SoundPlayer')
-        player.play_sound_file(birthday_with_name, vc)
+        vc = await self.connect(ctx)
+        to_speak = self.convert_args_to_tts_text(args)
+        name_sound = self.text_to_wav(to_speak)
+        birthday_with_name = self.birthday_sound(name_sound, ctx)
+        self.play_sound_file(birthday_with_name, vc)
 
     @commands.command()
     async def speakfile(self, ctx, *args):
         """Send the input text as a sound file from text-to-speech."""
         if not args:
             raise discord.InvalidArgument()
-        to_speak = self.strip_quotes(' '.join(args))  # ingest args
+        to_speak = self.convert_args_to_tts_text(args)
         await ctx.send(file=discord.File(self.text_to_wav(to_speak)))
 
     @commands.command()
@@ -98,37 +108,46 @@ class TTS(commands.Cog):
         """Text-to-speech but f a s t."""
         if not args:
             raise discord.InvalidArgument()
-        to_speak = self.strip_quotes(' '.join(args))  # ingest args
-        vc = await self.bot.get_cog('Control').connect_to_user(ctx)
-        self.play_text(vc, to_speak, speed='fast')
+        vc = await self.connect(ctx)
+        to_speak = self.convert_args_to_tts_text(args)
+        sound_file = self.make_tts_sound_file(to_speak, speed='fast')
+        self.play_sound_file(sound_file, vc)
 
     @commands.command()
     async def speak(self, ctx, *args):
         """Play your input text through text-to-speech."""
         if not args:
             raise discord.InvalidArgument()
-        to_speak = self.strip_quotes(' '.join(args))  # ingest args
-        vc = await self.bot.get_cog('Control').connect_to_user(ctx)
-        self.play_text(vc, to_speak)
+        vc = await self.connect(ctx)
+        to_speak = self.convert_args_to_tts_text(args)
+        sound_file = self.make_tts_sound_file(to_speak)
+        self.play_sound_file(sound_file, vc)
 
     @commands.command()
     async def speakdrunk(self, ctx, *args):
         """Text-to-speech but more drunk."""
         if not args:
             raise discord.InvalidArgument()
-        to_speak = self.strip_quotes(''.join(args))  # ingest args
-        vc = await self.bot.get_cog('Control').connect_to_user(ctx)
-        self.play_text(vc, to_speak, speed='slow')
+        vc = await self.connect(ctx)
+        to_speak = self.convert_args_to_tts_text(args)
+        to_speak = to_speak.strip(' ') # remove spaces to drunkify
+        sound_file = self.make_tts_sound_file(to_speak, speed='slow')
+        self.play_sound_file(sound_file, vc)
 
+    def args_to_jtts_text(self, words: list):
+        words_in = self.strip_quotes(' '.join(words))  # ingest args
+        words_and_punct = textconverter.split_to_words_and_punctuation(words_in)
+        return ' '.join(textconverter.mixed_lang_to_katakana(words_and_punct))
+
+    # TODO add length detection to deal with long strings (split or truncate)
     @commands.command(aliases=['sa'])
     async def speakanime(self, ctx, *args):
         if not args:
             raise discord.InvalidArgument()
-        words_in = self.strip_quotes(' '.join(args))  # ingest args
-        words_and_punct = textconverter.split_to_words_and_punctuation(words_in)
-        kana_to_speak = ' '.join(textconverter.mixed_lang_to_katakana(words_and_punct))
-        vc = await self.bot.get_cog('Control').connect_to_user(ctx)
-        self.play_text(vc, kana_to_speak, engine=self.jtts)
+        vc = await self.connect(ctx)
+        kana_to_speak = self.args_to_jtts_text(args)
+        sound_file = self.make_tts_sound_file(kana_to_speak, engine=self.jtts)
+        self.play_sound_file(sound_file, vc)
 
     # TODO hide if jtts is none
     # TODO make this respect per-guild preferences
