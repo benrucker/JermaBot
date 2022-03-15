c4feb7b8ef6816a875f6a0d9de79d8a1f4b4f490
Ben Rucker <brucker117@gmail.com>
2022-03-15 06:51:31 +0000

    Finally update start script for new env

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 cd jerma
 while true
-do python3 jerma.py -mycroft /home/chamber/github/mimic1/mimic \
+do python3 jerma.py -mycroft /home/ben/git/mimic1/mimic \
 	-mv ap \
-	-jv /home/chamber/open_jtalk/MMDAgent_Example-1.8/Voice/mei/mei_normal.htsvoice \
-	-jd /home/chamber/open_jtalk/open_jtalk_dic_utf_8-1.11
+	-jv /home/ben/jtalk/MMDAgent_Example-1.8/Voice/mei/mei_normal.htsvoice \
+	-jd /home/ben/jtalk/open_jtalk_dic_utf_8-1.11
 sleep 5
 done
