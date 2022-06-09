cb2c0a52ac595624b020801016ffaf67067f82f4
Ben Rucker <brucker117@gmail.com>
2022-06-09 18:41:16 -0400

    Change startup script to beta location

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd jerma
-/home/ben/git/jermabot/.venv/bin/python3.10 jerma.py -mycroft /home/ben/git/mimic1/mimic \
+/home/ben/git/jermabeta/.venv/bin/python3.10 main.py -mycroft /home/ben/git/mimic1/mimic \
 	-mv ap \
 	-jv /home/ben/jtalk/MMDAgent_Example-1.8/Voice/mei/mei_normal.htsvoice \
 	-jd /home/ben/jtalk/open_jtalk_dic_utf_8-1.11
